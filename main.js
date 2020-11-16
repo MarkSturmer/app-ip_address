a6e3dcfcb7ec95c6760ec71d4e912b5793463ee3
MarkSturmer <Mark.Sturmer@Rocketmail.com>
2020-11-16 17:45:57 +0000

    Lab 4

--- main.js
+++ main.js
@@ -38,54 +38,6 @@ const IPCIDR = require('ip-cidr');
  }
 
 
-/**
- * Calculate and return the first host IP address from a CIDR subnet.
- * @param {string} cidrStr - The IPv4 subnet expressed
- *                 in CIDR format.
- * @param {callback} callback - A callback function.
- * @return {Ipv4Ipv6} - If successful, this will be a Json object containing the (firstIPAddress, mappedIP6Address). (expecting exactly two values).
- */
-
-/*
-function getFirstIpAddress(cidrStr, callback) {
-
-  // Initialize return arguments for callback
-  let firstIpAddress = null;
-  let callbackError = null; 
-
-  let successfulRespJson = {ipv4:null,ipv6:null};
-
-  // Instantiate an object from the imported class and assign the instance to variable cidr.
-  const cidr = new IPCIDR(cidrStr);
-  // Initialize options for the toArray() method.
-  // We want an offset of one and a limit of one.
-  // This returns an array with a single element, the first host address from the subnet.
-  const options = {
-    from: 1,
-    limit: 1
-  };
- log.info('**** getFirstIpAddress input cidr = ' + cidr);
-  // Use the object's isValid() method to verify the passed CIDR.
-  if (!cidr.isValid()) {
-    // If the passed CIDR is invalid, set an error message.
-    callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
-  } else {
-    // If the passed CIDR is valid, call the object's toArray() method.
-    // Notice the destructering assignment syntax to get the value of the first array's element.
-    [firstIpAddress] = cidr.toArray(options);
-
-    //call to get the IPv6 for the returned IPv4
-    let respIPv6 = getIpv4MappedIpv6Address(firstIpAddress);
-    successfulRespJson = Ipv4Ipv6(firstIpAddress,respIPv6);
-  }
-  // Call the passed callback function.
-  // Node.js convention is to pass error data as the first argument to a callback.
-  // The IAP convention is to pass returned data as the first argument and error
-  // data as the second argument to the callback function.
-  return callback(successfulRespJson, callbackError);
-}
-*/
-
 class IpAddress {
     constructor() {
         // IAP's global log object is used to output errors, warnings, and other
@@ -97,7 +49,13 @@ class IpAddress {
         
     }
 
-//// added getFirstIpAddr START
+    /**
+    * Calculate and return the first host IP address from a CIDR subnet.
+    * @param {string} cidrStr - The IPv4 subnet expressed
+    *                 in CIDR format.
+    * @param {callback} callback - A callback function.
+    * @return {Ipv4Ipv6} - If successful, this will be a Json object containing the (firstIPAddress, mappedIP6Address). (expecting exactly two values).
+    */
     getFirstIpAddress(cidrStr, callback) {
 
         // Initialize return arguments for callback
@@ -135,7 +93,6 @@ class IpAddress {
         // data as the second argument to the callback function.
         return callback(successfulRespJson, callbackError);
     }
-//// added getFirstIpAddr END
 }
 
 module.exports = new IpAddress;
